bd6941ae90740b08bdc7f42614038fe3cfd60bde
hubbah arshad <133002842+hubbaha@users.noreply.github.com>
2025-01-21 21:30:26 +0500

    ..

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -10,27 +10,7 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ isSidebar, handleSidebar }) => {
   return (
     <>
-      <div className={`sidebar ${isSidebar ? "active" : ""}`}>
-        <div className="sidebar__overlay" onClick={handleSidebar}></div>
-        <div className="sidebar__content">
-          <button className="close-btn" onClick={handleSidebar}>
-            Close
-          </button>
-          <nav>
-            <ul>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/about">About</Link>
-              </li>
-              <li>
-                <Link href="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+     
     </>
   );
 };
